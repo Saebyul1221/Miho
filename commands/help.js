a9bd18421897bb396c729e1a8d8dbc8e88495400
CwhiteKJ <dw696969@naver.com>
2021-01-10 11:56:47 +0900

    fix: add index file

--- commands/help.js
+++ commands/help.js
@@ -2,11 +2,19 @@ module.exports.run = async (client, message, _args, _knex, embed) => {
   const commandDir = require("./")
 
   embed.setDescription(`${client.user.username}의 도움말이에요!\n\u200b`)
+  let i = 1
   for (const [key, value] of Object.entries(commandDir)) {
-    embed.addField(
-      "미호야 " + value.help.name,
-      value.help.description + "\n\u200b"
-    )
+    i % 2 == 0
+      ? embed.addField(
+          "미호야 " + value.help.name,
+          value.help.description + "\n\u200b",
+          true
+        )
+      : embed.addField(
+          "미호야 " + value.help.name,
+          value.help.description + "\n\u200b",
+          false
+        )
   }
 
   message.channel.send(`${message.member}`, { embed: embed })
---
+++ config/index.js
@@ -0,0 +1,2 @@
+module.exports.client = require("./client")
+module.exports.database = require("./database")
