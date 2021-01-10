9df08211cf1e634d7068fe1a5c0077f13c49c600
CwhiteKJ <dw696969@naver.com>
2021-01-10 15:08:12 +0900

    feat: remove handler, restore files

--- events/ready.js
+++
@@ -1,7 +0,0 @@
-module.exports = async (client) => {
-  console.log("=".repeat(40))
-  console.log(
-    `${client.user.username}#${client.user.discriminator} IS READY!\n\n전체 유저 수: ${client.users.cache.size}`
-  )
-  console.log("=".repeat(40))
-}
