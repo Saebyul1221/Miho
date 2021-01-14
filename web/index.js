b5d129a11ef84cccd8a5f7ae8411061c5e7806b3
CwhiteKJ <dw696969@naver.com>
2021-01-15 08:22:28 +0900

    fix: 경로를 찾을 수 없음

--- web/index.js
+++ web/index.js
@@ -1,6 +1,10 @@
+const path = require("path")
+const viewsPath = path.join(__dirname, "../views")
 const express = require("express")
 const app = express()
-app.use(express.static("public"))
+
+app.use(express.static(__dirname + "/public"))
+app.set("views", viewsPath)
 app.engine("html", require("ejs").renderFile)
 app.set("view engine", "html")
 
