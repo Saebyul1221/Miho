91400258ab735cd9cc053ea5519bd49dfe2be16c
CwhiteKJ <dw696969@naver.com>
2021-01-17 19:24:36 +0900

    fix:  단위수정

--- commands/wallet.js
+++ commands/wallet.js
@@ -15,17 +15,36 @@ module.exports.run = async (_client, message, _args, knex, embed, _data) => {
   let items = ""
   Object.keys(JSON.parse(user.items)).forEach((el) => {
     if (JSON.parse(user.items)[el] !== 0)
-      items += "\n" + st[el] + ": " + JSON.parse(user.items)[el] + " 주"
+      items += "\n" + st[el] + ": `" + JSON.parse(user.items)[el] + "` 주"
   })
 
   embed.setTitle(`${message.author.username}님의 지갑!\u200b`)
-  embed.setDescription(`추정 자산: \`${money + Number(user.money)}\`원\u200b`)
+  embed.setDescription(
+    `추정 자산: \`${comma(money + Number(user.money))}\`원\u200b`
+  )
   embed.addField(
     "현재 주식 정보",
     items.length === 0 ? "보유한 주식이 없어요." : items.replace("\n", "")
   )
 
   message.channel.send(`${message.member}`, { embed: embed })
+}
+
+function comma(num) {
+  let len, point, str
+
+  num = num + ""
+  point = num.length % 3
+  len = num.length
+
+  str = num.substring(0, point)
+  while (point < len) {
+    if (str != "") str += ","
+    str += num.substring(point, point + 3)
+    point += 3
+  }
+
+  return str
 }
 
 module.exports.help = {
--- utils/Stocks.js
+++ utils/Stocks.js
@@ -7,9 +7,9 @@ module.exports = class Stocks {
     let stocks = await this.knex("stocks").select("*")
 
     for (let i = 0; i < stocks.length; i++) {
-      let random = Math.floor(Math.random() * 10) + -10
+      let random = Math.floor(Math.random() * 20) + -10
       let now = Number(stocks[i].now) + random
-      if (now > 0) {
+      if (now > 50) {
         await this.knex("stocks")
           .update({
             now,
