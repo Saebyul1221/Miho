87b29b80c765afb25c6330935983cd7c202ce161
CwhiteKJ <dw696969@naver.com>
2021-01-22 10:28:22 +0900

    fix: 버그 수정

--- commands/eval.js
+++ commands/eval.js
@@ -9,36 +9,50 @@ module.exports.run = async (client, message, args, knex, embed) => {
       embed.setDescription("`process.exit()` 함수는 이용할 수 없습니다!")
       message.channel.send(`${message.member}`, { embed: embed })
     } else {
-      const result = new Promise((resolve) => resolve(eval(text)))
-      return result
-        .then((output) => {
-          if (typeof output !== "string")
-            output = require("util").inspect(output, {
-              depth: 0,
-            })
+      try {
+        const result = new Promise((resolve) => resolve(eval(text)))
+        return result
+          .then((output) => {
+            if (typeof output !== "string")
+              output = require("util").inspect(output, {
+                depth: 0,
+              })
 
-          if (output.includes(client.token))
-            output = output.replace(client.token, "토큰")
-          if (output.length > 1010) output = output.slice(0, 1010) + "\n..."
-          embed.setDescription(
-            "입력 :\n```js\n" + text + "\n```\n출력 :```js\n" + output + "\n```"
-          )
-          message.channel.send(`${message.member}`, { embed: embed })
-        })
-        .catch((error) => {
-          error = error.toString()
-          error = error.replace(client.token, "토큰")
-
-          if (error.includes(client.token))
+            if (output.includes(client.token))
+              output = output.replace(client.token, "토큰")
+            if (output.length > 1010) output = output.slice(0, 1010) + "\n..."
+            embed.setDescription(
+              "입력 :\n```js\n" +
+                text +
+                "\n```\n출력 :```js\n" +
+                output +
+                "\n```"
+            )
+            message.channel.send(`${message.member}`, { embed: embed })
+          })
+          .catch((error) => {
+            error = error.toString()
             error = error.replace(client.token, "토큰")
-          embed.setTitle("오류!")
-          embed.setDescription(`
+
+            if (error.includes(client.token))
+              error = error.replace(client.token, "토큰")
+            embed.setTitle("오류!")
+            embed.setDescription(`
+  \`\`\`js
+  ${error}
+  \`\`\`
+  `)
+            message.channel.send(`${message.member}`, { embed: embed })
+          })
+      } catch (err) {
+        embed.setTitle("오류!")
+        embed.setDescription(`
 \`\`\`js
-${error}
+${err}
 \`\`\`
 `)
-          message.channel.send(`${message.member}`, { embed: embed })
-        })
+        message.channel.send(`${message.member}`, { embed: embed })
+      }
     }
   } else {
     message.channel.send(
