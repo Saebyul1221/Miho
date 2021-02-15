6c6ac8555fe2a6379569fcf4a58d5a10542ef7e0
CwhiteKJ <dw696969@naver.com>
2021-02-15 16:41:46 +0900

    fix: 올바르지 않은 유저를 처리하지 못함

--- commands/profile.js
+++ commands/profile.js
@@ -3,19 +3,22 @@ module.exports.run = async (client, message, args, _knex, embed) => {
     args[1] !== undefined
       ? message.mentions.users.first()
         ? message.mentions.users.first()
-        : client.users.cache.get(args[1])
+        : checkUser(client, args[1])
       : message.author
-
-  if (!user) {
-    embed.setImage(user.displayAvatarURL({ size: 2048, dynamic: true }))
-    embed.setFooter(user.username + "님의 프로필!")
-    message.channel.send(`${message.member}`, { embed: embed })
-  }
   embed.setImage(user.displayAvatarURL({ size: 2048, dynamic: true }))
   embed.setFooter(user.username + "님의 프로필!")
   message.channel.send(`${message.member}`, { embed: embed })
 }
 
+function checkUser(client, user) {
+  if (user === undefined) throw new Error("유저 파라미터에 문제가 있습니다.")
+
+  let searchById = client.users.cache.get(user)
+  let searchByName = client.users.cache.find((u) => u.username === user)
+
+  if (searchById === undefined) return searchByName
+  else return searchById
+}
 module.exports.help = {
   name: "프로필",
   description: "해당 유저의 프로필을 확인합니다.",
